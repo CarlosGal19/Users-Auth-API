080a46d45fb2393d33f1ab06c5a0b9082a6afe5f
Carlos Galindo <utm22030587@utma.edu.mx>
2024-09-15 13:23:09 -0600

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace Users_Login_Api.Models
+
+{
+    using BCrypt.Net;
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHashed { get; set; }
+
+        public void HashPassword(string password)
+        {
+            var Salt = BCrypt.GenerateSalt();
+            PasswordHashed = BCrypt.HashPassword(password, Salt);
+        }
+
+        public bool VerifyPassword(string password)
+        {
+            return BCrypt.Verify(password, PasswordHashed);
+        }
+    }
+}
