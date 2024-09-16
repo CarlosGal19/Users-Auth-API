03f473bed681905e517a2dca1b28d4fdfde50ffb
Carlos Galindo <utm22030587@utma.edu.mx>
2024-09-16 17:43:43 -0600

    feat: create context

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Users_Login_Api.Models;
+
+namespace Users_Login_Api.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
