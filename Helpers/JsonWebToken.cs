5d060702c9510681d6b3200f9e6b44000c2d9964
Carlos Galindo <utm22030587@utma.edu.mx>
2024-09-16 17:44:37 -0600

    feat: create JsonWebToken class and generate jwt

---
+++ Helpers/JsonWebToken.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Users_Login_Api.Helpers
+{
+    public class JsonWebToken
+    {
+
+        public string Token { get; set; }
+
+        public string GenerateToken(string name)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("CarlosAlejandroGalindoIslasKarenIbethMorenoAguilar"));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, name),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "cg-api",
+                audience: "users",
+                claims: claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
